e5414e7c281d0668daa528c8d779df6b90c52507
Sylvain Leroux <sylvain@chicoree.fr>
2018-03-12 00:43:08 +0100

    Load pug templates to apply from user supplied location

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
-const adp = require('../index.js');
+const adp = require('../index.js')('./test/templates');
 const asciidoctor = require('asciidoctor.js')();
+const debug = require('debug')('asciidoctor.js-pug:tests');
 
 describe("asciidoctor", function() {
   it("should convert simple texts", function() {
     const doc = asciidoctor.loadFile('./test/data/001-plain-text.adoc');
     const html = doc.convert();
+    debug(html);
+  });
+
+  it("should find templates for block elements", function() {
+    const doc = asciidoctor.loadFile('./test/data/002-blocks.adoc');
+    const html = doc.convert();
+    debug(html);
+  });
+
+  it("should find templates for inline elements", function() {
+    const doc = asciidoctor.loadFile('./test/data/003-anchors.adoc');
+    const html = doc.convert();
+    debug(html);
   });
 });
