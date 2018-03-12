5e013d87a961d2164207ade8f5a137876c5890b6
Sylvain Leroux <sylvain@chicoree.fr>
2018-03-12 01:16:01 +0100

    Add tests for inline anchors

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const adp = require('../index.js')('./test/templates');
 const asciidoctor = require('asciidoctor.js')();
 const debug = require('debug')('asciidoctor.js-pug:tests');
+const assert = require('chai').assert;
 
 describe("asciidoctor", function() {
   it("should convert simple texts", function() {
@@ -12,12 +13,16 @@ describe("asciidoctor", function() {
   it("should find templates for block elements", function() {
     const doc = asciidoctor.loadFile('./test/data/002-blocks.adoc');
     const html = doc.convert();
+
     debug(html);
+    assert.include(html, '<PARA>');
   });
 
   it("should find templates for inline elements", function() {
     const doc = asciidoctor.loadFile('./test/data/003-anchors.adoc');
     const html = doc.convert();
     debug(html);
+    
+    assert.include(html, '<A HREF="http://asciidoctor.org">asciidoctor</A>');
   });
 });
